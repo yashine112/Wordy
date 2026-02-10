f9b8dde0afc3609d31411e53270128623c670ee6
yashine112 <salvatorescionti@googlemail.com>
2026-02-10 10:59:39 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "wort-generator-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
