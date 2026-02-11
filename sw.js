eece5f2e09879736f6aaa82d4842fb153b5963db
yashine112 <salvatorescionti@googlemail.com>
2026-02-11 11:58:06 +0100

    Aktualisieren von sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,31 @@
-const CACHE_NAME = "wort-generator-v6";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
+const CACHE_NAME = "wordy-v7"; // <- Zahl bei Änderungen hochzählen
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js"
+];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => (k !== CACHE_NAME) ? caches.delete(k) : null));
+    await self.clients.claim();
+  })());
+});
+
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
+  event.respondWith((async () => {
+    const cached = await caches.match(event.request);
+    if (cached) return cached;
+    const fresh = await fetch(event.request);
+    return fresh;
+  })());
 });
