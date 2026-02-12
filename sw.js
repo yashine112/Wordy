08ee8ecdb37e25b0d7935a5298b72b65c340bf24
yashine112 <salvatorescionti@googlemail.com>
2026-02-12 10:56:33 +0100

    Aktualisieren von sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "wordy-v7"; // <- Zahl bei Änderungen hochzählen
+const CACHE_NAME = "wordy-v8"; // <- Zahl bei Änderungen hochzählen
 const ASSETS = [
   "./",
   "./index.html",
